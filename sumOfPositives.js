47a011b51a18b19ab51b6952d896f1827157f7c4
Charles Grandison <therealakeem03@gmail.com>
2022-07-20 10:34:19 +0100

    Added refactored code

--- sumOfPositives.js
+++ sumOfPositives.js
@@ -2,4 +2,9 @@ function positiveSum(arr) {
     return arr.map((i) => (i >= 0 ? i : null)).reduce((a, c) => a + c, 0);
 }
 
+// Refactored Code
+// function positiveSum(arr) {
+//   return arr.reduce((a, b) => a + (b > 0 ? b : 0), 0);
+// }
+
 console.log(positiveSum([1, -4, 7, 12]))
