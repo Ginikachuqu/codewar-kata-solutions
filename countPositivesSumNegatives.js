25fb5fb17205b475cf74679e860e888544ec678c
Charles Grandison <therealakeem03@gmail.com>
2022-07-20 21:29:26 +0100

    Resolved previous challenge

--- countPositivesSumNegatives.js
+++ countPositivesSumNegatives.js
@@ -1,10 +1,8 @@
-// Unresolved
-
 function countPositivesSumNegatives(input) {
     const isAllZero = (cur) => cur === 0
 
-    if (input === [] || input.every(isAllZero)) {
-        return 'IV'
+    if (input === [] || input == null || input.every(isAllZero)) {
+        return []
     } else {
         const positives = input.filter((i) => i > 0);
         const negatives = input.filter((i) => i < 0);
@@ -15,9 +13,9 @@ function countPositivesSumNegatives(input) {
     }
 }
 
-// console.log(
-//   countPositivesSumNegatives([
-//     0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14,
-//   ])
-// );
-console.log(countPositivesSumNegatives([0, 0]))
+console.log(
+  countPositivesSumNegatives([
+    0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14,
+  ])
+);
+// console.log(countPositivesSumNegatives([0, 0]))
