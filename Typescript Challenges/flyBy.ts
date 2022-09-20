1f4eea697ea8faa5f0a23167930a7845e6a02c54
Charles Grandison <therealakeem03@gmail.com>
2022-09-20 20:28:33 +0100

    Added new challenge

---
+++ Typescript Challenges/flyBy.ts
@@ -0,0 +1,14 @@
+// The other day I saw an amazing video where a guy hacked some wifi controlled lightbulbs by flying a drone past them. Brilliant.
+// In this kata we will recreate that stunt... sort of.
+// You will be given two strings: lamps and drone. lamps represents a row of lamps, currently off,
+// each represented by x. When these lamps are on, they should be represented by o.
+// The drone string represents the position of the drone T (any better suggestion for character??) and its flight path up until this point =.
+// The drone always flies left to right, and always begins at the start of the row of lamps. Anywhere the drone has flown, including its current position,
+// will result in the lamp at that position switching on.
+
+// Solution
+function flyBy(lamps: string, drone: string): string {
+    return lamps.split('').fill('o', 0, drone.length).join('')
+}
+
+console.log(flyBy('xxxxxxxxx', '==T'))
