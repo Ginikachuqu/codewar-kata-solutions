4f8102c7452e43608a7d55e38d67cb6974329591
Charles Grandison <therealakeem03@gmail.com>
2022-09-21 10:09:32 +0100

    Solved pending challenge

--- Typescript Challenges/countBits.js
+++ Typescript Challenges/countBits.js
@@ -1,6 +1,10 @@
+// Write a function that takes an integer as input,
+// and returns the number of bits that are equal to
+// one in the binary representation of that number.
+// You can guarantee that input is non-negative.
 function countBits(n) {
     var num = [];
-    var dec = (n >>> 0).toString(2).split('').map(function (x) { return x == '1' ? num.push(x) : null; });
+    var dec = n.toString(2).split('').map(function (x) { return x == '1' ? num.push(x) : null; });
     return num.length;
 }
-console.log(countBits(10));
+console.log(countBits(3339522749242465));
--- Typescript Challenges/countBits.ts
+++ Typescript Challenges/countBits.ts
@@ -1,9 +1,12 @@
-// Write a function that takes an integer as input, and returns the number of bits that are equal to one in the binary representation of that number. You can guarantee that input is non-negative.
+// Write a function that takes an integer as input,
+// and returns the number of bits that are equal to
+// one in the binary representation of that number.
+// You can guarantee that input is non-negative.
 
-function countBits(n: number): number {
+function countBits(n: number): any {
     let num = []
-    let dec = (n >>> 0).toString(2).split('').map(x => x == '1' ? num.push(x) : null)
+    let dec = n.toString(2).split('').map(x => x == '1' ? num.push(x) : null)
     return num.length
 }
 
-console.log(countBits(10))
+console.log(countBits(3339522749242465))
